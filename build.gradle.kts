a3628f67b338cf04aefe2ef2418a3eef62cf680e
Froze-N-Milk <oscar.chevalier@gmail.com>
2025-04-03 16:53:30 +1100

    feat: added sloth and run configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,14 @@
 plugins {
-	id("dev.frozenmilk.teamcode") version "10.1.1-0.1.3"
+	id("dev.frozenmilk.teamcode") version "10.2.0-0.1.3"
+	id("dev.frozenmilk.sinister.sloth.load") version "0.2.1"
+}
+
+repositories {
+	maven("https://repo.dairy.foundation/releases")
+}
+
+dependencies {
+	implementation("dev.frozenmilk.sinister:Sloth:0.2.1")
 }
 
 ftc {
