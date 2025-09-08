87f5e99948e35d4ca5ebe6c8b2fb0badb83d50a2
Froze-N-Milk <oscar.chevalier@gmail.com>
2025-09-08 15:43:20 +1000

    chore: updated libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-	id("dev.frozenmilk.teamcode") version "10.2.0-0.1.3"
-	id("dev.frozenmilk.sinister.sloth.load") version "0.2.2"
+	id("dev.frozenmilk.teamcode") version "10.3.0-0.1.4"
+	id("dev.frozenmilk.sinister.sloth.load") version "0.2.4"
 }
 
 repositories {
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-	implementation("dev.frozenmilk.sinister:Sloth:0.2.2")
+	implementation("dev.frozenmilk.sinister:Sloth:0.2.4")
 }
 
 ftc {
